997864b5a7b062dce4fae91b287e3ec6c16f983b
Andrew Senner <me@andrewsenner.com>
2017-10-02 12:45:21 -0400

    User model defined w/ attributes

---
+++ back-end/models/User.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const uniqueValidator = require('mongoose-unique-validator');
+const createSignedSessionId = require('../services/session');
+
+const UserSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		passwordHash: String,
+		profile: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Profile"
+		},
+		classes: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Class"
+		}],
+		tasks: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Task"
+		}],
+		rewards: [{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "Reward"
+		}]
+		notifications: [{
+			message: {
+				type: String,
+				required: true
+			},
+			type: {
+				type: String,
+				required: true
+			},
+		}]
+
+		sessionToken: String
+	},
+	{
+		timestamps: true
+	}
+);
+
+UserSchema.plugin(uniqueValidator);
+
+UserSchema.virtual('fullname').get(function() {
+	return this.fname + ' ' + this.lname;
+});
+
+UserSchema.virtual('password').set(function(val) {
+	this.passwordHash = bcrypt.hashSync(val, 10);
+});
+
+UserSchema.methods.validatePassword = function(password) {
+	return bcrypt.compareSync(password, this.passwordHash);
+};
+
+UserSchema.pre('save', function(next) {
+	this.sessionToken = createSignedSessionId(this.username);
+	next();
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
