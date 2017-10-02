aabe510de814d16ad347fe3f78107c3cc6f2e4f2
idhalverson <halversoncode@gmail.com>
2017-10-02 17:15:49 -0400

    working on classroom router

--- back-end/routers/classes.js
+++ back-end/routers/classes.js
@@ -1,13 +1,80 @@
 const router = require("express").Router();
+const { Classroom } = require("../models");
+const { createResponse } = require("../server/util");
 
-// add student to class
-router.post("/classes/:classId/students", (req, res) => {});
+// creating a classroom
+router.post("/", async (req, res) => {
+  try {
+    const { kind, description, cost, teacher, status } = req.body;
+    const classroom = new Classroom({
+      kind,
+      description,
+      cost,
+      teacher,
+      status
+    });
+    await classroom.save();
+    res.json(createResponse(classroom));
+  } catch (error) {
+    console.error(error);
+    res.json(createResponse(error));
+  }
+});
 
-router.get("/", (req, res) => {});
+title: {
+  type: String,
+  required: true,
+  unique: true
+},
+description: {
+  type: String,
+  required: true
+},
+students: [
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Student'
+  }
+],
+teachers: [
+  {
+    type: mongoose.Schema.Types.ObjectId,
 
-router.patch("/", (req, res) => {});
 
-// remove student from class
-router.delete("/classes/:classId/students/:studentId", (req, res) => {});
+// reading a classroom
+router.get("/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const classroom = await Classroom.findById(_id);
+    res.json(createResponse(classroom));
+  } catch (error) {
+    res.json(createResponse(error));
+  }
+});
+
+// updating a classroom
+router.patch("/:id", async (req, res) => {
+  try {
+    const { updates } = req.body;
+    const _id = req.params.id;
+    const classroom = await Classroom.findByIdAndUpdate(_id, updates);
+    res.json(createResponse(classroom));
+  } catch (error) {
+    console.error(error);
+    res.json(createResponse(error));
+  }
+});
+
+// deleting a classroom
+router.delete("/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const classroom = await Classroom.findByIdAndRemove(_id);
+    res.json(createResponse(classroom));
+  } catch (error) {
+    console.log(error);
+    res.json(createResponse(error));
+  }
+});
 
 module.exports = router;
--- back-end/routers/tasks.js
+++ back-end/routers/tasks.js
@@ -7,55 +7,13 @@ router.post("/", async (req, res) => {
   try {
     const { title, description, rewards, teacher, class, status } = req.body;
     const task = new Task({ title, description, rewards, teacher, class, status });
-    const profileParams = {
-      title,
-      displayName: email.split("@")[0],
-      avatar: null,
-      gender,
-      fname,
-      lname
-    };
-    const profile = new Profile(profileParams);
-    task.profile = profile;
     await task.save();
-    await profile.save();
-    await req.login(task, () => {});
     res.json(createResponse(task));
   } catch (error) {
     console.error(error);
     res.json(createResponse(error));
   }
 });
-
-title: {
-  type: String,
-  required: true,
-  unique: true
-},
-description: {
-  type: String,
-  required: true
-},
-rewards: [
-  {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Reward'
-  }
-],
-teacher: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'Teacher'
-},
-class: {
-  type: mongoose.Schema.Types.ObjectId,
-  ref: 'Class'
-},
-status: {
-  type: String,
-  default: 'Unassigned'
-}
-},
-
 
 // reading a task
 router.get("/:id", async (req, res) => {
