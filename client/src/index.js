db05a004fefe6a5db5ea7ba5ec19de5a5940c8ab
Stephanie Barker <song_mage@yahoo.com>
2017-10-02 11:13:18 -0400

    react boilerplate setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, applyMiddleware(thunk));
+
+const app = () => {
+	return (
+		<Provider>
+			<App />
+		</Provider>
+	);
+};
+
+ReactDOM.render(app, document.getElementById("root"));
 registerServiceWorker();
---
+++ client/src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+const reducer = (state, action) => {
+	switch (action.data) {
+		default:
+			return state;
+	}
+};
+
+export default reducer;
