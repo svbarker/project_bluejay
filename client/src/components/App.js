c7bb22e51d89c8a024356433b995f4806fca0875
Eric Glover <etgt28@mail.missouri.edu>
2017-10-02 17:42:04 -0500

    slight fix

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import TeacherNavbarContainer from "../containers/TeacherNavbar";
+import TeacherNavbarContainer from "../containers/TeacherNavbarContainer";
 import StudentNavbarContainer from "../containers/StudentNavbarContainer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { withRouter } from "react-router";
@@ -9,38 +9,38 @@ const TeacherNavbarContainerWithRouter = withRouter(TeacherNavbarContainer);
 const StudentNavbarContainerWithRouter = withRouter(StudentNavbarContainer);
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<Router>
-					<div>
-						<StudentNavbarContainerWithRouter />
-						<Switch>
-							{/* do some login checking here */}
-							<Route exact path="/">
-								<div>
-									<h1>Dashboard</h1>
-									<DashboardMenu />
-								</div>
-							</Route>
-							<Route path="/student">
-								<h1>Student</h1>
-							</Route>
-							<Route path="/task">
-								<h1>Tasks</h1>
-							</Route>
-							<Route path="/reward">
-								<h1>Rewards</h1>
-							</Route>
-							<Route path="/report">
-								<h1>Reports</h1>
-							</Route>
-						</Switch>
-					</div>
-				</Router>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <div>
+            <StudentNavbarContainerWithRouter />
+            <Switch>
+              {/* do some login checking here */}
+              <Route exact path="/">
+                <div>
+                  <h1>Dashboard</h1>
+                  <DashboardMenu />
+                </div>
+              </Route>
+              <Route path="/student">
+                <h1>Student</h1>
+              </Route>
+              <Route path="/task">
+                <h1>Tasks</h1>
+              </Route>
+              <Route path="/reward">
+                <h1>Rewards</h1>
+              </Route>
+              <Route path="/report">
+                <h1>Reports</h1>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
