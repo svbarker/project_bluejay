72cb286c3ec6bb257e78a0409726e8ffb4c9c0cc
Eric Glover <etgt28@mail.missouri.edu>
2017-10-02 20:20:03 -0500

    added appropriate styles

--- client/src/components/DashboardMenu.js
+++ client/src/components/DashboardMenu.js
@@ -4,6 +4,8 @@ import "../styles/DashboardMenu.css";
 import { Link } from "react-router-dom";
 import Paper from "material-ui/Paper";
 
+//THOUGHTS: It'd be nice to scale down the icons when on smaller screens
+
 const DashboardMenu = () => {
   return (
     <Paper style={{ backgroundColor: "#ff6161" }} className="dashboard-menu">
@@ -19,10 +21,7 @@ const DashboardMenu = () => {
         <Paper id="dashboard-menu-item2" className="dashboard-menu-item">
           <div className="dashboard-menu-grid">
             <h3 id="dashboard-item-higher">Tasks</h3>
-            {/* <i id="dashboard-item-lower" className="material-icons">
-              insert icon
-            </i> */}
-            <i className="fa fa-graduation-cap fa-5x" />
+            <i className="fa fa-tasks fa-5x" />
           </div>
         </Paper>
       </Link>
@@ -30,7 +29,7 @@ const DashboardMenu = () => {
         <Paper id="dashboard-menu-item3" className="dashboard-menu-item">
           <div className="dashboard-menu-grid">
             <h3>Rewards</h3>
-            <i className="fa fa-graduation-cap fa-5x" />
+            <i className="fa fa-gift fa-5x" />
           </div>
         </Paper>
       </Link>
