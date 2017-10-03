0ffe832b2a0600410c32e2958816917cb0e9d34d
Stephanie Barker <song_mage@yahoo.com>
2017-10-03 19:14:56 -0400

    started students route

---
+++ client/src/components/StudentList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class StudentList extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {}
+
+	render() {
+		return <p>Hi</p>;
+	}
+}
+
+export default StudentList;
