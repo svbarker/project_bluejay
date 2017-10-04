bded4ef3ae10951b6ab1a7b591730e5f835f5933
Stephanie Barker <song_mage@yahoo.com>
2017-10-04 13:28:06 -0400

    displaying tasks on the student page

---
+++ client/src/components/TaskAssignList.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import AssignableTask from "./AssignableTask";
+import { Card, CardHeader, CardText } from "material-ui/Card";
+
+class TaskAssignList extends Component {
+	constructor(props) {
+		super(props);
+	}
+	componentDidMount() {
+		this.props.hydrateTasks(this.props.userId);
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Tasks</h2>
+				<Card>
+					<CardHeader
+						actAsExpander={true}
+						showExpandableButton={true}
+						style={{
+							"background-color": "#1a8484"
+						}}
+						iconStyle={{ color: "white" }}
+						title="Tasks"
+						titleStyle={{
+							color: "white",
+							"font-weight": "bold",
+							"font-size": "16px"
+						}}
+					/>
+					<CardText expandable={true}>
+						<div className="task-card-container">
+							{this.props.tasks.map(task => {
+								return <AssignableTask task={task} />;
+							})}
+						</div>
+					</CardText>
+				</Card>
+			</div>
+		);
+	}
+}
+
+export default TaskAssignList;
