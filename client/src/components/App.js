e8f5e1fa3ed5b4c2c22d9e21591f38a6f6ddb2c9
Eric Glover <etgt28@mail.missouri.edu>
2017-10-05 12:45:08 -0500

    deleting, toggling Availability for rewards

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,8 +19,8 @@ import PageNotFound from "./PageNotFound";
 const TeacherNavbarContainerWithRouter = withRouter(TeacherNavbarContainer);
 const StudentNavbarContainerWithRouter = withRouter(StudentNavbarContainer);
 
-const userType = "Student";
-// const userType = "Teacher";
+// const userType = "Student";
+const userType = "Teacher";
 
 class App extends Component {
   componentDidMount() {
