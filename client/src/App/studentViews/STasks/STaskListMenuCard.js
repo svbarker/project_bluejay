1e3130ed4c883a39d1aeee182aa965d40b119bbb
idhalverson <halversoncode@gmail.com>
2017-10-11 17:11:25 -0400

    Tasksstyling

--- client/src/App/studentViews/STasks/STaskListMenuCard.js
+++ client/src/App/studentViews/STasks/STaskListMenuCard.js
@@ -9,118 +9,120 @@ import Chip from "material-ui/Chip";
 import FlatButton from "material-ui/FlatButton";
 //styles don't work, idk why
 const rewardsChipHeader = {
-	gridRowStart: "1",
-	gridRowEnd: "2",
-	textAlign: "center",
-	padding: "10px"
+  gridRowStart: "1",
+  gridRowEnd: "2",
+  textAlign: "center",
+  padding: "10px"
 };
 const rewardsChipContainer = {
-	display: "grid",
-	gridTemplateRows: "1fr 2fr"
+  display: "grid",
+  gridTemplateRows: "1fr 2fr"
 };
 
 const rewardsChip = {
-	height: "50px",
-	fontSize: "8px"
+  height: "50px",
+  fontSize: "8px"
 };
 const rewardsChipP = {
-	fontSize: "12px"
+  fontSize: "12px"
 };
 const rewardsChipList = {
-	display: "flex",
-	flexDirection: "row",
-	justifyContent: "center",
-	flexWrap: "wrap",
-	gridRowStart: "2",
-	padding: "8px"
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  flexWrap: "wrap",
+  gridRowStart: "2",
+  padding: "8px"
 };
 const BUTTON_MARK_COMPLETED_TEXT = "Mark Completed";
 const BUTTON_PENDING_TEXT = "Pending Teacher Approval";
 class StudentTaskListMenuCard extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			label: props.task.pending
-				? BUTTON_PENDING_TEXT
-				: BUTTON_MARK_COMPLETED_TEXT,
-			visible: true,
-			disabled: props.task.pending
-		};
-	}
-	componentDidMount() {
-		///
-	}
-	render() {
-		const { task, markCompleted, user, socket } = this.props;
-		const { _id, title, value, description, classroom } = task;
-		const rewards = task.rewards.map(reward => {
-			return (
-				<Chip key={reward._id} style={rewardsChip}>
-					<Avatar src="images/rewards/rewards1.png" />
-					<p>
-						{reward.title}
-					</p>
-				</Chip>
-			);
-		});
-		return (
-			<Card style={{ marginBottom: "20px", backgroundColor: "#85DCDC" }}>
-				<CardHeader
-					actAsExpander={true}
-					showExpandableButton={true}
-					title={title}
-					style={{
-						backgroundColor: "#1a8484"
-					}}
-					iconStyle={{ color: "white" }}
-					titleStyle={{ color: "white", fontWeight: "bold" }}
-				/>
-				<CardText expandable={true}>
-					<Paper style={{ padding: "20px" }}>
-						<div className="menu-card-container">
-							<p>
-								{description}
-							</p>
-							{this.props.task.status !== "CompletedTask"
-								? <div className="menu-card-button-container">
-										<FlatButton
-											disabled={this.state.disabled}
-											label={this.state.label}
-											style={{ color: "rgb(255,255,255)" }}
-											onClick={() => {
-												if (!this.state.disabled)
-													markCompleted(user.id, _id, socket);
-												this.setState({
-													label: BUTTON_PENDING_TEXT,
-													disabled: true
-												});
-											}}
-											backgroundColor="#1a8484"
-											hoverColor="#3ca6a6"
-										/>
-									</div>
-								: ""}
-						</div>
-					</Paper>
-					<Divider />
-					<Paper style={{ marginTop: "20px" }}>
-						<div
-							style={rewardsChipContainer}
-							className="rewards-chip-container"
-						>
-							{/* center this */}
-							<div style={rewardsChipHeader} className="rewards-chip-header">
-								<h3>Rewards Given For Completing This Task</h3>
-							</div>
-							<div style={rewardsChipList} className="rewards-chip-list">
-								{rewards}
-							</div>
-						</div>
-					</Paper>
-				</CardText>
-			</Card>
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      label: props.task.pending
+        ? BUTTON_PENDING_TEXT
+        : BUTTON_MARK_COMPLETED_TEXT,
+      visible: true,
+      disabled: props.task.pending
+    };
+  }
+  componentDidMount() {
+    ///
+  }
+  render() {
+    const { task, markCompleted, user, socket } = this.props;
+    const { _id, title, value, description, classroom } = task;
+    const rewards = task.rewards.map(reward => {
+      return (
+        <Chip key={reward._id} style={rewardsChip}>
+          <Avatar src="images/rewards/rewards1.png" />
+          <p>{reward.title}</p>
+        </Chip>
+      );
+    });
+    return (
+      <Card style={{ marginBottom: "20px", backgroundColor: "#85DCDC" }}>
+        <CardHeader
+          actAsExpander={true}
+          showExpandableButton={true}
+          title={title}
+          style={{
+            backgroundColor: "#1a8484"
+          }}
+          iconStyle={{ color: "white" }}
+          titleStyle={{ color: "white", fontWeight: "bold" }}
+        />
+        <CardText expandable={true}>
+          <Paper style={{ padding: "20px" }}>
+            <div className="menu-card-container">
+              <div>
+                <h3>{title}</h3>
+                <hr />
+                <p>{description}</p>
+              </div>
+              {this.props.task.status !== "CompletedTask" ? (
+                <div className="menu-card-button-container">
+                  <FlatButton
+                    disabled={this.state.disabled}
+                    label={this.state.label}
+                    style={{ color: "rgb(255,255,255)" }}
+                    onClick={() => {
+                      if (!this.state.disabled)
+                        markCompleted(user.id, _id, socket);
+                      this.setState({
+                        label: BUTTON_PENDING_TEXT,
+                        disabled: true
+                      });
+                    }}
+                    backgroundColor="#1a8484"
+                    hoverColor="#3ca6a6"
+                  />
+                </div>
+              ) : (
+                ""
+              )}
+            </div>
+          </Paper>
+          <Divider />
+          <Paper style={{ marginTop: "20px" }}>
+            <div
+              style={rewardsChipContainer}
+              className="rewards-chip-container"
+            >
+              {/* center this */}
+              <div style={rewardsChipHeader} className="rewards-chip-header">
+                <h3>Rewards Given For Completing This Task</h3>
+              </div>
+              <div style={rewardsChipList} className="rewards-chip-list">
+                {rewards}
+              </div>
+            </div>
+          </Paper>
+        </CardText>
+      </Card>
+    );
+  }
 }
 
 export default StudentTaskListMenuCard;
