895ef130c65755062e78f2cc3e29b8f09c3f9a94
Andrew Senner <me@andrewsenner.com>
2017-10-11 17:36:11 -0400

    Fix missed links in logging system

--- back-end/routers/sessions.js
+++ back-end/routers/sessions.js
@@ -8,64 +8,65 @@ const { User } = require("../models");
 
 // passport login route
 router.post("/", async (req, res, next) => {
-  try {
-    await passport.authenticate("local", async (err, user, info) => {
-      try {
-        if (!user) {
-          throw new Error(
-            `Invalid email or password. (email: ${req.body.username})`
-          );
-        }
-        await req.logIn(user, err => {
-          // Create log event.
-          logEvent(UserEvent, {
-            message: Messages.TEMPLATE_LOGGED_IN,
-            owner: user,
-            user
-          });
-        });
-        res.json(createResponse(user));
-      } catch (error) {
-        logError(error);
-        res.json(createResponse(error));
-      }
-    })(req, res, next);
-  } catch (error) {
-    logError(error);
-    res.json(createResponse(error));
-  }
+	try {
+		await passport.authenticate("local", async (err, user, info) => {
+			try {
+				if (!user) {
+					throw new Error(
+						`Invalid email or password. (email: ${req.body.username})`
+					);
+				}
+				await req.logIn(user, err => {
+					// Create log event.
+					logEvent(UserEvent, {
+						message: Messages.TEMPLATE_LOGGED_IN,
+						owner: user,
+						user
+					});
+				});
+				res.json(createResponse(user));
+			} catch (error) {
+				logError(error);
+				res.json(createResponse(error));
+			}
+		})(req, res, next);
+	} catch (error) {
+		logError(error);
+		res.json(createResponse(error));
+	}
 });
 
 //get data to hydrate front end if user has persisting session
 router.get("/", mw.authCheck, async (req, res, next) => {
-  try {
-    if (!req.user) {
-      return res.end();
-    }
+	try {
+		if (!req.user) {
+			return res.end();
+		}
 
-    const user = await User.findById(req.user.id);
-    res.json(createResponse(user));
-  } catch (error) {
-    logError(error);
-    res.json(createResponse(error));
-  }
+		const user = await User.findById(req.user.id);
+		res.json(createResponse(user));
+	} catch (error) {
+		logError(error);
+		res.json(createResponse(error));
+	}
 });
 
 router.delete("/", async (req, res, next) => {
-  // Create log event.
-  logEvent(UserEvent, {
-    message: Messages.TEMPLATE_LOGGED_OUT,
-    owner: req.user
-  });
+	// Create log event.
+	logEvent(UserEvent, {
+		message: Messages.TEMPLATE_LOGGED_OUT,
+		owner: req.user,
+		user: req.user
+	});
 
-  try {
-    await req.logout(() => {});
-    res.cookie("connect.sid", "", { expires: new Date() });
-    res.json(createResponse());
-  } catch (error) {
-    logError(error);
-    res.json(createResponse(error));
-  }
+	try {
+		await req.logout(() => {});
+		res.cookie("connect.sid", "", { expires: new Date() });
+		res.json(createResponse());
+	} catch (error) {
+		logError(error);
+		res.json(createResponse(error));
+	}
 });
 
 module.exports = router;
