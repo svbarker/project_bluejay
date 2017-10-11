9d4578642024832d4ad6490da77eb035635e5af4
idhalverson <halversoncode@gmail.com>
2017-10-11 16:15:18 -0400

    rewards styling

--- client/src/App/studentViews/SRewards/SRewardCard.js
+++ client/src/App/studentViews/SRewards/SRewardCard.js
@@ -10,73 +10,69 @@ import Paper from "material-ui/Paper";
 import RaisedButton from "material-ui/RaisedButton";
 
 class RewardCard extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	//when the edit modal sends an edit add the reward id
-	onSubmit = rewardUpdates =>
-		this.props.editTask(this.props.reward._id, rewardUpdates);
+  //when the edit modal sends an edit add the reward id
+  onSubmit = rewardUpdates =>
+    this.props.editTask(this.props.reward._id, rewardUpdates);
 
-	//what is this
-	render() {
-		const {
-			_id,
-			title,
-			value,
-			description,
-			classroom,
-			cost,
-			supply,
-			available
-		} = this.props.reward;
+  //what is this
+  render() {
+    const {
+      _id,
+      title,
+      value,
+      description,
+      classroom,
+      cost,
+      supply,
+      available
+    } = this.props.reward;
 
-		return (
-			<Card style={{ marginBottom: "20px" }}>
-				<CardHeader
-					actAsExpander={true}
-					showExpandableButton={true}
-					title={`(${cost || value || "None"}) ${title}`}
-					style={{
-						backgroundColor: "#96cd28"
-					}}
-					iconStyle={{ color: "white" }}
-					titleStyle={{ color: "white", fontWeight: "bold" }}
-				/>
-				<CardText expandable={true}>
-					<Paper style={{ padding: "20px" }}>
-						<p>
-							Description: {description || "None"}
-						</p>
-						<p>
-							Kind of reward: {cost ? "Loot" : "Point"}
-						</p>
-						<p>
-							Cost: {cost || value || "None"}
-						</p>
-						<p>
-							Available: {available ? "YES" : "NO"}
-						</p>
-						<p>
-							Supply: {supply || "Unlimited"}
-						</p>
-						<Undoable
-							disabled={this.props.points < cost ? true : false}
-							resolve={() => this.props.onPurchase(_id)}
-							wait={2}
-						>
-							<FlatButton
-								// onClick={() => this.props.onPurchase(reward)}
-								disabled={this.props.points < cost ? true : false}
-								primary={this.props.points > cost ? true : false}
-								label="purchase"
-							/>
-						</Undoable>
-					</Paper>
-				</CardText>
-			</Card>
-		);
-	}
+    return (
+      <Card style={{ marginBottom: "20px" }}>
+        <CardHeader
+          actAsExpander={true}
+          showExpandableButton={true}
+          title={`${title} (${cost || "?"} Points)`}
+          style={{
+            backgroundColor: "#96cd28"
+          }}
+          iconStyle={{ color: "white" }}
+          titleStyle={{ color: "white", fontWeight: "bold" }}
+        />
+        <CardText expandable={true}>
+          <Paper style={{ padding: "20px" }}>
+            <h2>{title}</h2>
+            <h4>{cost || "?"} Points</h4>
+            <hr />
+            <h3>Description: </h3>
+            <p>{description || "None"}</p>
+            <p>
+              <strong>
+                This reward is{" "}
+                {available ? "available." : "not available right now."}
+              </strong>
+            </p>
+            <Undoable
+              disabled={this.props.points < cost ? true : false}
+              resolve={() => this.props.onPurchase(_id)}
+              wait={2}
+            >
+              <FlatButton
+                // onClick={() => this.props.onPurchase(reward)}
+                disabled={this.props.points < cost ? true : false}
+                primary={this.props.points > cost ? true : false}
+                label="purchase"
+              />
+            </Undoable>
+          </Paper>
+        </CardText>
+      </Card>
+    );
+  }
 }
 
 export default RewardCard;
