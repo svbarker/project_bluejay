548aabc1e1d54dff1b282a87143ebc96161d5414
idhalverson <halversoncode@gmail.com>
2017-10-12 12:18:29 -0400

    register styling

--- client/src/App/GlobalComponents/Register.js
+++ client/src/App/GlobalComponents/Register.js
@@ -8,94 +8,112 @@ import { Link } from "react-router-dom";
 import "../Styles/Login.css";
 
 const Register = ({
-	onKeyDown,
-	handleSubmit,
-	handlePasswordChange,
-	handleSelectChange,
-	selectValue,
-	validate,
-	fnameError,
-	lnameError,
-	emailError,
-	passwordError,
-	passwordConfirmError,
-	submitError
+  onKeyDown,
+  handleSubmit,
+  handlePasswordChange,
+  handleSelectChange,
+  selectValue,
+  validate,
+  fnameError,
+  lnameError,
+  emailError,
+  passwordError,
+  passwordConfirmError,
+  submitError
 }) => {
-	return (
-		<div className="registration-container">
-			<Paper style={{ padding: "20px" }}>
-				<h2>Register your teacher account</h2>
-				{!submitError
-					? null
-					: <span>A user with that email already exists.</span>}
-				<form
-					className="registration-form-container"
-					onSubmit={handleSubmit}
-					onKeyDown={onKeyDown}
-				>
-					<div>
-						<fieldset>
-							<legend>Personal Information</legend>
-							<TextField
-								floatingLabelText="First Name"
-								errorText={fnameError}
-								id="fname"
-								onBlur={validate}
-							/>
-							<TextField
-								floatingLabelText="Last Name"
-								errorText={lnameError}
-								id="lname"
-								onBlur={validate}
-							/>
-							<SelectField
-								floatingLabelText="Title"
-								onChange={handleSelectChange}
-								value={selectValue}
-								id="title"
-							>
-								<MenuItem value={"Miss"} primaryText="Miss" />
-								<MenuItem value={"Mr."} primaryText="Mr." />
-								<MenuItem value={"Mrs."} primaryText="Mrs." />
-								<MenuItem value={"Ms."} primaryText="Ms." />
-							</SelectField>
-							<TextField
-								type="email"
-								floatingLabelText="Email"
-								errorText={emailError}
-								id="email"
-								onBlur={validate}
-							/>
-						</fieldset>
-						<p>
-							Already have an account? <Link to="/login">Login</Link>
-						</p>
-					</div>
-					<div>
-						<fieldset>
-							<legend>Account Credentials</legend>
-							<TextField
-								type="password"
-								floatingLabelText="Password"
-								errorText={passwordError}
-								id="password"
-								onChange={handlePasswordChange}
-								onBlur={validate}
-							/>
-							<TextField
-								type="password"
-								floatingLabelText="Confirm Password"
-								errorText={passwordConfirmError}
-								id="passwordConfirm"
-								onBlur={validate}
-							/>
-						</fieldset>
-						<RaisedButton label="Create Account" type="submit" />
-					</div>
-				</form>
-			</Paper>
-		</div>
-	);
+  return (
+    <div className="registration-container">
+      <Paper style={{ padding: "20px" }}>
+        <h2>Register:</h2>
+        {!submitError ? null : (
+          <span>A user with that email already exists.</span>
+        )}
+        <form
+          className="registration-form-container"
+          onSubmit={handleSubmit}
+          onKeyDown={onKeyDown}
+        >
+          <div>
+            <fieldset>
+              <legend>Personal Information</legend>
+              <TextField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                floatingLabelText="First Name"
+                errorText={fnameError}
+                id="fname"
+                onBlur={validate}
+              />
+              <TextField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                floatingLabelText="Last Name"
+                errorText={lnameError}
+                id="lname"
+                onBlur={validate}
+              />
+              <SelectField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                floatingLabelText="Title"
+                onChange={handleSelectChange}
+                value={selectValue}
+                id="title"
+              >
+                <MenuItem value={"Miss"} primaryText="Miss" />
+                <MenuItem value={"Mr."} primaryText="Mr." />
+                <MenuItem value={"Mrs."} primaryText="Mrs." />
+                <MenuItem value={"Ms."} primaryText="Ms." />
+              </SelectField>
+              <TextField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                type="email"
+                floatingLabelText="Email"
+                errorText={emailError}
+                id="email"
+                onBlur={validate}
+              />
+            </fieldset>
+            <p>
+              Already have an account? <Link to="/login">Login</Link>
+            </p>
+          </div>
+          <div>
+            <fieldset>
+              <legend>Account Credentials</legend>
+              <TextField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                type="password"
+                floatingLabelText="Password"
+                errorText={passwordError}
+                id="password"
+                onChange={handlePasswordChange}
+                onBlur={validate}
+              />
+              <TextField
+                floatingLabelStyle={{ color: "grey" }}
+                underlineFocusStyle={{ borderColor: "#1A8484" }}
+                type="password"
+                floatingLabelText="Confirm Password"
+                errorText={passwordConfirmError}
+                id="passwordConfirm"
+                onBlur={validate}
+              />
+            </fieldset>
+            <RaisedButton
+              backgroundColor={"#1A8484"}
+              labelColor={"white"}
+              style={{ margin: "20px 20px 20px 5px" }}
+              label="Create Account"
+              type="submit"
+            />
+          </div>
+        </form>
+      </Paper>
+    </div>
+  );
 };
 
 export default Register;
