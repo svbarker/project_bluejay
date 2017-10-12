71268126e8e0cec405447b8cb8b9279eeb78f804
idhalverson <halversoncode@gmail.com>
2017-10-12 18:57:44 -0400

    coming soon pages

---
+++ client/src/App/teacherViews/TReports/TReports.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Paper from "material-ui/Paper";
+
+const TReports = () => {
+  return (
+    <div className="notifications-container">
+      <h1>Reports</h1>
+      <Paper style={{ padding: "4px", borderRadius: "22px" }}>
+        <div className="notifications-container-inner">
+          <h3 style={{ textAlign: "center" }}>Coming Soon</h3>
+        </div>
+      </Paper>
+    </div>
+  );
+};
+
+export default TReports;
