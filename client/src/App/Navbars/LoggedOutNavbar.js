3db9c2b749661d5bf6358ff8e3c000601f52f170
idhalverson <halversoncode@gmail.com>
2017-10-12 12:10:27 -0400

    bluejay title

--- client/src/App/Navbars/LoggedOutNavbar.js
+++ client/src/App/Navbars/LoggedOutNavbar.js
@@ -1,15 +1,13 @@
 import React from "react";
 
 const LoggedOutNavbar = () => {
-	return (
-		<div className="navbar navbar-base">
-			<div className="navbar-left">
-				<h1>
-					{"Kids' Productivity App"}
-				</h1>
-			</div>
-		</div>
-	);
+  return (
+    <div className="navbar navbar-base">
+      <div className="navbar-left">
+        <h1 style={{ fontSize: "30px" }}>{"bluejay"}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default LoggedOutNavbar;
